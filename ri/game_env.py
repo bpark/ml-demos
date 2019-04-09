f396138377ac5154a4fd0494e37e4cf74d8d0036
bpark <burt.parkers@gmail.com>
2019-04-09 22:12:23 +0200

    added Actor and Abilities

--- ri/game_env.py
+++ ri/game_env.py
@@ -1,16 +1,51 @@
+from abc import ABCMeta, abstractmethod
 from random import randint
+from typing import List
+
+
+class Actor:
+
+    def __init__(self, id, total_hp: int, base_dmg: int, ab: List) -> None:
+        super().__init__()
+        self.id = id
+        self.total_hp = total_hp
+        self.base_dmg = base_dmg
+        self.ab = ab
+        self.effects = {
+            "dmg": 0,
+            "total_hp": total_hp,
+            "current_hp": total_hp
+        }
+
+    def apply(self, ab_index: int) -> None:
+        self.ab[ab_index].apply(actor=self)
+
+
+class Ability(metaclass=ABCMeta):
+
+    @abstractmethod
+    def apply(self, actor: Actor) -> None:
+        pass
+
+
+class Strike(Ability):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def apply(self, actor: Actor) -> None:
+        actor.effects["dmg"] = actor.base_dmg
+        print("Performing Strike with dmg: " + str(actor.effects["dmg"]))
 
 
 class GameEnv:
 
-    ATTACK = 0
-    HUNT = 1
-    FLEE = 2
-
-    def __init__(self, health, dmg) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self.health = health
-        self.dmg = dmg
+        self.actors = {}
+
+    def add_actor(self, actor: Actor) -> None:
+        self.actors[actor.id] = actor
 
     # next_state, reward, done
     def step(self, action) -> tuple:
@@ -18,3 +53,7 @@ class GameEnv:
 
     def sample(self) -> int:
         return randint(0, 2)
+
+
+a1 = Actor(1, 500, 50, ab=[Strike()])
+a1.apply(0)
